0d3f43bc6c87a2ab95a16bc1056564685e6921ab
kscarby <sabrina.keito@gmail.com>
2025-07-25 18:26:22 -0300

    feat terms and privacity

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import { register } from '../services/authService';
 import { useNavigate } from 'react-router-dom';
 
+import "../styles/Register.css";
+
 export default function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [acepted, setAcepted] = useState(false);
   const navigate = useNavigate();
   const [error, setError] = useState('');
 
@@ -20,7 +23,7 @@ export default function Register() {
   };
 
   return (
-    <div className='container-form'>
+    <div className='container-form-login'>
       <form className='form-login' onSubmit={handleSubmit}>
         <h2 className='form-h2'>Criar Conta</h2>
         <input 
@@ -39,6 +42,20 @@ export default function Register() {
           onChange={e => setPassword(e.target.value)} 
           required 
         />
+        <div className='form-terms'>
+          <input 
+            className='form-input-terms'
+            type="checkbox"
+            value={acepted}
+            onChange={e => setAcepted(e.target.value)} 
+            required 
+          />
+          <p className='p-terms'>Eu aceito os </p>
+          <button className='bt-terms'>termos de uso</button><p className='p-terms'> e </p>
+          <button className='bt-terms'>política de privacidade</button>
+          
+        </div>
+
         <button className='button-buy' type="submit">Criar Conta</button>
         {error && <p style={{color:'red'}}>{error}</p>}
       </form>
